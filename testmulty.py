381c9118115beb7397c57ac1b2e07e65bdf3094e
cm1320 <doyun.shin@gmail.com>
2019-07-25 14:55:48 +0900

    Create testmulty.py

---
+++ testmulty.py
@@ -0,0 +1,26 @@
+from multiprocessing import process
+
+def func1():
+    print('func1111')
+    print('f11')
+    print('f11111111111111111')
+
+def func2():
+    print('f2')
+    print('f222')
+    print('f21312322')
+
+
+
+def main():
+    p1 = Process(target=func1)
+    p2 = Process(target=func2)
+
+    p1.start
+    p2.start
+
+
+
+if __name__ == '__main__':
+
+    main()
